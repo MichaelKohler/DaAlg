c3e54c7df87062c3f45aeb53c9e0af0047d84ad2
Michael Kohler <michaelkohler@live.com>
2012-02-22 23:53:56 +0100

    Übung1: Source Code of Praktische Aufgabe

--- Uebung1/SortTester.java
+++ Uebung1/SortTester.java
@@ -1,10 +1,16 @@
 /**
  * File: SortTester.java
- *
+ * Autoren:
+ *   - Lukas Diener - 
+ *   - Michael Kohler - 11-108-289
  * Einfaches Testprogramm der Sortieralgorithmen.
  */
 
 public class SortTester {
+
+    public static final int MAX_EXPERIMENT_TIME_IN_MS = 600000;
+    public static final int MAX_LENGTH = 100000;
+    public static final int ELEMENT_UPPERLIMIT = 1000000;
 
 	public static void printArray(int[] array)
 	{
@@ -15,17 +21,39 @@ public class SortTester {
 	}
 
 	public static void main(String[] args) {
+        Timer experimentTimer = new Timer();
+        while(experimentTimer.timeElapsed()<MAX_EXPERIMENT_TIME_IN_MS) {
+		    // zu sortierender Array generieren
+            int[] array = generateNewArray();
+            int[] array2 = new int[array.length];
+		    java.lang.System.arraycopy(array, 0, array2, 0, array.length);
 
-		// zu sortierender Array:
-		int[] array = {5, 2, 9, 1, 2, 7, 3, 2, 4, 8, 1};
-		int[] array2 = new int[array.length];
-		java.lang.System.arraycopy(array, 0, array2, 0, array.length);
+            // beginn Insertion Sort
+            Timer timer = new Timer();
+		    Sorting.insertionSort(array);
+		    long time = timer.timeElapsed();
+		    System.out.println(time+"\t ms to insertion sort\t"+
+		                        array.length+"\telements: ");
+		    printArray(array);
+		    // beginn Merge Sort
+		    timer.reset();
+		    Sorting.mergeSort(array2, 0, array.length-1);
+		    long time2 = timer.timeElapsed();
+		    System.out.println(time2+"\t ms to merge sort\t"+
+		                        array2.length+"\telements: ");
+		    printArray(array2);
+        }
+	}
 
-		Sorting.insertionSort(array);
-		printArray(array);
-		Sorting.mergeSort(array2, 0, array.length-1);
-		printArray(array2);
-
+	public static int[] generateNewArray() {
+	    java.util.Random random = new java.util.Random();
+        int size = random.nextInt(MAX_LENGTH);
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            int randomNumber = random.nextInt(ELEMENT_UPPERLIMIT);
+            array[i] = randomNumber;
+        }
+        return array;
 	}
 }
 
